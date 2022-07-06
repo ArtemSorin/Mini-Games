12223d8871a9a0b269682287e81d26a77de16369
ArtemSorin <95878097+ArtemSorin@users.noreply.github.com>
2022-07-06 07:53:43 +0300

    Добавьте файлы проекта.

---
+++ Card Game/Card Game/Page.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Card_Game
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page : ContentPage
+    {
+        public Page()
+        {
+            InitializeComponent();
+        }
+        async public void function1(object sender, EventArgs e)
+        {
+            var player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+            player.Load("card.mp3");
+            player.Play();
+            await btn2.RotateYTo(90, 300);
+            await btn1.RotateYTo(0, 300);
+        }
+        async public void function2(object sender, EventArgs e)
+        {
+            var player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+            player.Load("card.mp3");
+            player.Play();
+            await btn1.RotateYTo(90, 300);
+            await btn2.RotateYTo(0, 300);
+        }
+    }
+}
